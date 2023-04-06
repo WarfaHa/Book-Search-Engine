a9916353f1a468acccd929bd9bf9f7887dbfd77e
Warfa <warfahassan35@yahoo.com>
2023-04-05 23:21:42 -0400

    Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,27 +5,59 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+
+        return userData;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
+
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { email, password }) => {
+      const user = await user.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No profile with this email found!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
+
+    saveBook: async (parent, { userId, bookData }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: userId },
+          { 
+            $addToSet: { savedBooks: { book: bookData } } 
+          },
+          { 
+            new: true, 
+            runValidators: true 
+          }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
   },
+
 };
 
 module.exports = resolvers;
