fc19a9a21171e41b6c2e781330a614800729bf7c
Warfa <warfahassan35@yahoo.com>
2023-04-06 01:42:30 -0400

    fixed remove book mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,4 +44,22 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-// Remove Book
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
